fe230865c08b094285cc45f3be80abf715efe0d6
progr <programmingcvv@gmail.com>
2026-04-13 08:38:51 -0500

    READ AN INTEGER AND DETERMINE IF IT'S NEGATIVE PHP - OK 13-abr-26

---
+++ 06.PHP/pg-166-169/pg_166_03.php
@@ -0,0 +1,21 @@
+<?php
+
+echo("");
+echo("PHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPH<br>");
+echo("READ AN INTEGER AND DETERMINE IF IT'S NEGATIVE<br>");
+echo("PHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPH<br><br>");
+echo("");
+
+$num = 933;
+echo($num . "<br><br>");
+
+if($num < 0)
+    {
+        echo("The writen number is negative: {$num}");
+    }
+    else
+        {
+            echo("The written number ISN'T negative: {$num}");
+        }
+
+?>
