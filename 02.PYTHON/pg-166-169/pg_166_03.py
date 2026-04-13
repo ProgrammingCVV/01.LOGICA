7c67d8051e7f05090b625a4ce8afd786953f3756
progr <programmingcvv@gmail.com>
2026-04-13 06:55:55 -0500

    READ AN INTEGER AND DETERMINE IF IT'S NEGATIVE - ok 13-abr-26

---
+++ 02.PYTHON/pg-166-169/pg_166_03.py
@@ -0,0 +1,12 @@
+print("");
+print("PYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPY");
+print("READ AN INTEGER AND DETERMINE IF IT'S NEGATIVE");
+print("PYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPY");
+print("");
+
+num = int(input("Write an integer: "));
+
+if(num < 0):
+    print("The written number is negative: ", num);
+else:
+    print("The written number ISN'T negative: ", num);
