0d02e7f5d8093438cea51845e8324d3040c5127d
progr <programmingcvv@gmail.com>
2026-04-13 09:41:38 -0500

    Carga parcial del fichero a repo remoto.

--- 01.JAVA/pg-166-169/pg_166_04.java
+++ 01.JAVA/pg-166-169/pg_166_04.java
@@ -1,42 +1,28 @@
 import java.util.Scanner;
-public class pg_166_04{
-    public static void main(String[] args){
+public class pg_166_04
+{
+    public static void main(String[] args)
+    {
+        Scanner enter = new Scanner(System.in);
+        int num ;
 
         System.out.println("");
-        System.out.println("******************************************************************************************");
-        System.out.println("LEER UN NÚMERO ENTERO DE DOS DÍGITOS Y DETERMINAR A CUÁNTO ES IGUAL LA SUMA DE SUS DÍGITOS");
-        System.out.println("******************************************************************************************");
+        System.out.println("JVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJ");
+        System.out.println("READ A TWO-DIGIT INTEGER AND DETERMINE WHAT THE SUM OF ITS DIGITS IS EQUAL TO");
+        System.out.println("JVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJ");
         System.out.println("");
 
-        int num = 0;
-        int Ud  = 0;
-        int PUd = 0;
+        System.out.print("Write an integer with two digits: ");
+        num = enter.nextInt();
 
-        Scanner entrada = new Scanner(System.in);
-
-        System.out.print("Escriba un número entero de 2 dígitos:  ");
-        num = entrada.nextInt();
-        System.out.println("");
-
-        if(num <= 0){
-            num = num*(-1);
+        if(num < 0)
+        {
+            num *= -1;
         }
 
-        if(num >= 10 && num <= 99){
-            System.out.println("El número escrito es :" + num);
-
-            Ud = num - num / 10 * 10;
-            System.out.println("El último dígito es:  " + Ud);
-
-            PUd = (num / 10) % 10;
-            System.out.println("El penúltimo dígito es:  " + PUd);
+        while(num > 0)
+        {
             
-            System.out.println("La suma de estos dos dígitos es:  " + ( Ud + PUd));
-
-
-
-        }else{
-            System.out.println("En número escrito NO TIENE 2 DIGITOS. Intente nuevamente.");
         }
 
     }
