1c4ea6dd40d8ab567e9b8ab5606f0e7dbba7955c
SKYNET <programmingcvv@gmail.com>
2026-04-18 15:11:51 -0500

    READ A TWO-DIGITS INTEGER AND DETERMINE IF BOTH ARE EVEN PHP - OK 18-ABR-26

---
+++ 06.PHP/pg-166-169/pg_166_05.php
@@ -0,0 +1,37 @@
+<?php
+echo("");
+echo("PHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPPHPHPHPHPHPHPHPHPHPHPHPHPH<br>");
+echo("READ A TOW-DIGITS INTEGER AND DETERMINE IF BOTH DIGITS ARE EVEN<br>");
+echo("PHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPPHPHPHPHPHPHPHPHPHPHPHPHPH<br><br>");
+echo("");
+
+$num = 85;
+$dig1 = 0;
+$dig2 = 0;
+
+echo($num . "<br>");
+
+if($num < 0)
+    {
+        $num *= -1;
+    }
+
+if($num >= 10 & $num <=99)
+    {
+        $dig2 = $num % 10;
+        $dig1 = intdiv($num, 10);
+
+        if($dig1 % 2 == 0 & $dig2 % 2 == 0)
+            {
+                echo("Both digits are even.");
+            }
+            else
+                {
+                    echo("Both digits are not even.");
+                }
+    }
+    else
+        {
+            echo("The written number doesn't have two digits. Please try again!");
+        }
+?>
