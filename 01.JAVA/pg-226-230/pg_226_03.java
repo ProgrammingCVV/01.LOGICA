32a93ff0bca7f91a89a5eb78bd20e32254fa2a45
SKYNET <programmingcvv@gmail.com>
2026-05-09 09:57:47 -0500

    READ AN INTEGER AND DISPLAY ALL ITS EXACT DIVISORS BETWEEN 1 AND THE GIVEN NUMBER JAVA - OK 09-MAY-26

--- 01.JAVA/pg-226-230/pg_226_03.java
+++ 01.JAVA/pg-226-230/pg_226_03.java
@@ -21,7 +21,7 @@ public class pg_226_03
             {
                 num *= -1;
             }
-
+/*
             while(i <= num)
             {
                 if(num % i == 0)
@@ -29,8 +29,17 @@ public class pg_226_03
                     System.out.println(i);
                 }
             i++;
-                
             }
+*/
+
+            for(i=1; i<= num; i++)
+            {
+                if(num % i == 0)
+                {
+                    System.out.print(i + ", ");
+                }
+            }
+                
         }
     }
 }
