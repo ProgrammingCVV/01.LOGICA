8c5fa535bbfe6e682a87db23b39916bb860c0aff
SKYNET <programmingcvv@gmail.com>
2026-05-08 20:08:46 -0500

    READ AN INTEGER AND DISPLAY ALL ITS EXACT DIVISORS BETWEEN 1 AND THE GIVEN NUMBER JAVA - OK 08-MAY-26

---
+++ 01.JAVA/pg-226-230/pg_226_03.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class pg_226_03
+{
+    public static void main(String[] args) {
+        {
+            Scanner enter = new Scanner(System.in);
+            int num;
+            int i = 1;
+
+            System.out.println("");
+            System.out.println("JVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJ");
+            System.out.println("READ AN INTEGER AND DISPLAY ALL ITS EXACT DIVISORS BETWEEN 1 AND THE GIVEN NUMBER");
+            System.out.println("JVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJ");
+            System.out.println("");
+
+            System.out.print("Write an iteger: ");
+            num = enter.nextInt();
+
+            if(num < 0)
+            {
+                num *= -1;
+            }
+
+            while(i <= num)
+            {
+                if(num % i == 0)
+                {
+                    System.out.println(i);
+                }
+            i++;
+                
+            }
+        }
+    }
+}
