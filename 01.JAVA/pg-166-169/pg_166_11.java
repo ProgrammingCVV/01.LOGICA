62f4ba61f00261d88b334b0aaa6ae93a64fa329d
SKYNET <programmingcvv@gmail.com>
2026-05-14 22:03:06 -0500

    READ TWO INTEGERS AND DETERMINE WICH IS THE GREATER JAVA - OK 14-MAY-26

---
+++ 01.JAVA/pg-166-169/pg_166_11.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class pg_166_11
+{
+    public static void main(String[]args)
+    {
+        Scanner enter = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.println("");
+        System.out.println("JVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJ");
+        System.out.println("READ TWO INTEGERS AND DETERMINE WICH IS THE GREATER");
+        System.out.println("JVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJVJ");
+        System.out.println("");
+
+        System.out.print("Write the first number: ");
+        num1 = enter.nextInt();
+
+        System.out.print("Write the second number: ");
+        num2 = enter.nextInt();
+
+        if(num1 < num2)
+        {
+            System.out.println("The first number is greater than the second.");
+        }
+        else
+        {
+            System.out.println("The second number is greater than the first.");
+        }
+
+    }
+}
