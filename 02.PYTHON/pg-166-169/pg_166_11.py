0fc18102f7335dc746b3370102f05dbaee7afec2
SKYNET <programmingcvv@gmail.com>
2026-05-14 22:17:10 -0500

    READ TWO INTEGERS AND DETERMINE WICH IS THE GREATER PY - OK 14-MAY-26

---
+++ 02.PYTHON/pg-166-169/pg_166_11.py
@@ -0,0 +1,13 @@
+print("");
+print("PYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYP");
+print("READ TWO INTEGERS AND DETERMINE WICH IS THE GREATER");
+print("PYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYP");
+print("");
+
+num1 = int(input("Write the first number: "));
+num2 = int(input("Write the second number: "));
+
+if(num1 > num2):
+    print("The first number is greater than the second.");
+else:
+    print("The second number is greater than the first.");
