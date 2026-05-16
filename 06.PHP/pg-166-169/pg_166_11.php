a5c38df0d9b6afa9ab84f28365bb4f9a80c828d8
SKYNET <programmingcvv@gmail.com>
2026-05-16 18:04:54 -0500

    READ TWO INTEGERS AND DETERMINE WHICH IS GREATER PHPH - OK 16-MAY-26

---
+++ 06.PHP/pg-166-169/pg_166_11.php
@@ -0,0 +1,23 @@
+<?php
+echo("");
+echo("PHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPH<br>");
+echo("READ TWO INTEGERS AND DETERMINE WHICH IS GREATER<br>");
+echo("PHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPHPH<br><br>");
+echo("");
+
+$num1 = 223;
+$num2 = 1273;
+
+echo($num1. "<br>");
+echo($num2. "<br>");
+
+if($num1 > $num2)
+    {
+        echo("The first number is greater than the second.");
+    }
+    else
+        {
+            echo("The second number is greater than the first.");
+        }
+
+?>
