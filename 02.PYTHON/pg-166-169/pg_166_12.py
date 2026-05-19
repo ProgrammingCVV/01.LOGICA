d932ca47ac2414433574c5c24e1b6f33386fbe5d
SKYNET <programmingcvv@gmail.com>
2026-05-18 19:55:25 -0500

    READ TWO TWO-DIGIT INTEGERS AND DETERMINE IF THEY BOTHS HAVE DIGITS IN COMMON PY - OK 18-MAY-26

--- 02.PYTHON/pg-166-169/pg_166_12.py
+++ 02.PYTHON/pg-166-169/pg_166_12.py
@@ -1,31 +1,32 @@
-print("")
-print("*******************************************************************************")
-print("READ TWO INTEGER NUMBERS OF TWO DIGITS AND DETERMINE IF THEY HAVE COMMON DIGITS")
-print("*******************************************************************************")
-print("")
+print("");
+print("PYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPY");
+print("READ TWO TWO-DIGIT INTEGERS AND DETERMINE IF THEY BOTH HAVE DIGITS IN COMMON");
+print("PYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPY");
+print("");
 
-num1 = int(input("Write the first number with two digits: "))
-num2 = int(input("Write the second number with two digits: "))
+dig1 = 0;
+dig2 = 0;
+dig3 = 0;
+dig4 = 0;
+num1 = int(input("Write the first digit: "));
+num2 = int(input("Write the second digit: "));
 
 if(num1 < 0):
-    num1 = num1 * (-1)
+    num1 *= -1;
+
 if(num2 < 0):
-    num2 = num2 * (-1)
+    num2 *= -1;
 
-if(num1 >=10 and num1 <= 99):
-    num1dig1 = (num1 // 10) % 10
-    num1dig2 = num1 - (num1 // 10 * 10)
+if(num1 >= 10 and num1 <= 99 and num2 >= 10 and num2 <= 99):
+    dig2 = num1 % 10;
+    dig1 = num1 // 10;
+    dig4 = num2 % 10;
+    dig3 = num2 // 10;
 
-    if(num2 >= 10 and num2 <= 99):
-        num2dig1 = (num2 // 10) % 10
-        num2dig2 = num2 - (num2 // 10 * 10)
-
-        if(num1dig1 == num2dig1 or num1dig1 == num2dig2 or num1dig2 == num2dig1 or num1dig2 == num2dig2):
-            print("The written numbers do have common digits.")
-        else:
-            print("The written numbers don't have common digits.")
+    if(dig1 == dig3 or dig1 == dig4 or dig2 == dig3 or dig2 == dig4):
+        print("There are digits in common between both writen numbers.");
     else:
-        print("The second number doesn't have two digits. Try again please.")
-else:
-    print("The first number doesn't have two digits. Try again please.")
+        print("There are not digits in common between both writen numbers.");
 
+else:
+    print("Some of the writen numbers doesn't have two digits. Please try again!");
