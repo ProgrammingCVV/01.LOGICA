36c551fa900833ed68239049a36a6772029bd32a
SKYNET <programmingcvv@gmail.com>
2026-05-28 21:05:10 -0500

    READ TWO TWO-DIGIT INTEGERS AND DETERMINE IF THE SUM OF BOTH INTEGERS PRODUCE AN EVEN NUMBER pg_166_13.py - 28-may-26

--- 02.PYTHON/pg-166-169/pg_166_13.py
+++ 02.PYTHON/pg-166-169/pg_166_13.py
@@ -1,29 +1,21 @@
-print("")
-print("**************************************************************************************************")
-print("READ TWO INTEGER NUMBERS WITH TWO DIGITS AND DETERMINE IF THE SUM OF BOTH ORIGINATE AN EVEN NUMBER")
-print("**************************************************************************************************")
-print("")
+print("");
+print("PYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPY");
+print("READ TWO TWO-DIGIT INTEGERS AND DETERMINE IF THE SUM OF BOTH INTEGERS PRODUCE AN EVEN NUMBER");
+print("PYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPYPY");
+print("");
 
-num1 = int(input("Write the first number: "))
-num2 = int(input("Write the second number: "))
+num1 = int(input("write the first integer: "));
+num2 = int(input("write the second integer: "));
 
 if(num1 < 0):
-    num1 = num1 * (-1)
+    num1 *= -1;
+
 if(num2 < 0):
-    num2 = num2 * (-1)
+    num2 *= -1;
 
-if(num1 >= 10 and num1 <= 99):
-    if(num2 >= 10 and num2 <= 99):
+sum = num1 + num2;
 
-        result = num1 + num2
-
-        if(result == result // 2 * 2):
-            print("The result {} is an even number.".format(result))    
-        else:
-            print("The result {} is an odd number.".format(result))
-
-    else:
-        print("Some number does not have two digits. Try again please.")
-
+if(sum % 2 == 0):
+    print("THE SUM OF BOTH INTEGERS (", sum, ") PRODUCES AN EVEN NUMBER");
 else:
-    print("Some number doesn't have two digits. Try again please.")
+    print("THE SUM OF BOTH INTEGERS (", sum, ") DOESN'T PRODUCE AN EVEN NUMBER");
